3143aee3871bcbc7d9f9fff325b864b7d97e941c
Effyiex <leakforumse@gmail.com>
2024-04-29 12:43:18 +0200

    UI Begin & Project Restructure

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+
 using AlgorithmicSumoBlazor;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +8,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
 await builder.Build().RunAsync();
